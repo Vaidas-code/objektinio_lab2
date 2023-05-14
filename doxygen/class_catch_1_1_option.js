3ad796cf6469292dc2e3b7e1c0ffbb20e3cba4e9
Vaidas-code <116501117+Vaidas-code@users.noreply.github.com>
2023-05-14 23:50:49 +0300

    Add files via upload

---
+++ doxygen/class_catch_1_1_option.js
@@ -0,0 +1,23 @@
+var class_catch_1_1_option =
+[
+    [ "Option", "class_catch_1_1_option.html#a8efb01b593d798decc80cbbdf311f2a3", null ],
+    [ "Option", "class_catch_1_1_option.html#a5aeb9c22d48a6882bdf5fb4730b06c86", null ],
+    [ "Option", "class_catch_1_1_option.html#af02f2e4559f06384baec0def8c68c5fd", null ],
+    [ "~Option", "class_catch_1_1_option.html#a37fe90bb47bb909f150a5ad6be25581a", null ],
+    [ "none", "class_catch_1_1_option.html#a821753afdc3fac947a13a01fbe0d248e", null ],
+    [ "operator SafeBool::type", "class_catch_1_1_option.html#a8ed8de7b072f893c85df14913dbbe197", null ],
+    [ "operator!", "class_catch_1_1_option.html#a96dccb86bdf45ee0c08e122b6133bef3", null ],
+    [ "operator*", "class_catch_1_1_option.html#ac250ca96c9af3bf5f94f253f4c0ec336", null ],
+    [ "operator*", "class_catch_1_1_option.html#a873a00ed8844b137287f25c811cfd330", null ],
+    [ "operator->", "class_catch_1_1_option.html#a04da75b6b0946cc38b4bbd51c8d407e0", null ],
+    [ "operator->", "class_catch_1_1_option.html#acb3ec06778a8761e5f0014543d8f898e", null ],
+    [ "operator=", "class_catch_1_1_option.html#a960fd629f3048995038d58fcf9c018b0", null ],
+    [ "operator=", "class_catch_1_1_option.html#a32af4018dd8aaf071944f79fc1af520a", null ],
+    [ "reset", "class_catch_1_1_option.html#a37b4e0e5d4d56296adacd267a616f4e0", null ],
+    [ "some", "class_catch_1_1_option.html#a97c95829afbe92f2bcc5fd75b32c0825", null ],
+    [ "valueOr", "class_catch_1_1_option.html#a8d9ae2e30b0eb76fe134a6fbc8423124", null ],
+    [ "dummy1", "class_catch_1_1_option.html#a48ef08179923d6f943abe614999e6073", null ],
+    [ "dummy2", "class_catch_1_1_option.html#afa73c2396b7732932a148b3c889b81d1", null ],
+    [ "dummy3", "class_catch_1_1_option.html#a5540a84662093591be496c3a57e9da3f", null ],
+    [ "storage", "class_catch_1_1_option.html#acdebca1b18bb8542c3f676b8dd805f23", null ]
+];
